91f70679c7cf244d16fa9cef055b3e45345e15c4
Manjeet Gulaniya <145897864+manjeetgulaniya@users.noreply.github.com>
2025-09-17 17:33:46 +0000

    Task 1

---
+++ task1.py
@@ -0,0 +1,9 @@
+a= int(input("Enter the first number\t"))
+b= int(input("Enter the second number\t"))
+
+print("Addition:\t"+str(a+b))
+print("Substraction:\t"+str(a-b))
+print("Multiplication:\t"+str(a*b))
+print("Division:\t"+str(a/b))
+
+#This code Written By Manjeet on 17sep 2025
